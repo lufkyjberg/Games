9161b985aff3d16b1d1019cf2e1dd270277a9672
Smurfik <glotov0000@bk.ru>
2025-11-01 02:42:01 +0400

    hzz

--- Brickbreaker.js
+++ Brickbreaker.js
@@ -1,8 +1,11 @@
 function Main(){
     document.getElementById();
-    let answer = column play 
     if(answer == false){
         console.log("Test");
         alert(answer);
     }
+}
+
+class BallShooter{
+    
 }
